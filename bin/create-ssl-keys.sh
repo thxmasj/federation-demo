670c2f6e38737c55a2d3efa00b72c0b66536d14d
thomasjo <ekst_thj@eika.no>
2014-09-16 15:18:56 +0200

    Added script for creating SSL keys for HTTPS communication between parties.

---
+++ bin/create-ssl-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p target/keys
+cd target/keys
+rm *
+
+# SSL: IdP
+keytool -genkeypair -validity 730 -alias idp-ssl -keystore idp-ssl.jks -dname "cn=localhost" -storepass changeit -keypass changeit
+keytool -export -file idp-ssl.cer -alias idp-ssl -keystore idp-ssl.jks -storepass changeit
+
+# SSL: RP
+keytool -genkeypair -validity 730 -alias rp-ssl -keystore rp-ssl.jks -dname "cn=localhost" -storepass changeit -keypass changeit
+keytool -export -file rp-ssl.cer -alias rp-ssl -keystore rp-ssl.jks -storepass changeit
+
+# SSL: WSP
+keytool -genkeypair -validity 730 -alias wsp-ssl -keystore wsp-ssl.jks -dname "cn=localhost" -storepass changeit -keypass changeit
+keytool -export -file wsp-ssl.cer -alias wsp-ssl -keystore wsp-ssl.jks -storepass changeit
