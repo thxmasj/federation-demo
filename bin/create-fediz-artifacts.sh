93d03b9c8cca6631c203f9101a418084a45a60d0
Thomas Johansen <thomas@Thomass-MacBook-Pro.local>
2014-09-17 08:47:00 +0200

    fixes

--- bin/create-fediz-artifacts.sh
+++ bin/create-fediz-artifacts.sh
@@ -3,6 +3,7 @@
 DIR=target/cxf-fediz
 rm -rf $DIR
 git clone -v https://git-wip-us.apache.org/repos/asf/cxf-fediz.git $DIR
+git checkout tags/fediz-1.1.1
 sed -i .old 's/^realmA.port=.*/realmA.port=8443/' $DIR/services/idp/src/main/filters/realm-a/env.properties
 sed -i .old 's/^realmB.port=.*/realmB.port=8443/' $DIR/services/idp/src/main/filters/realm-a/env.properties
 cd $DIR
