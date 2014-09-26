61aeed54dd696026bf303690b1f91397efa27df5
Thomas Johansen <thxmasj@gmail.com>
2014-09-26 13:27:46 +0200

    Forgot to commit...

--- hello/src/test/java/ClassloaderTest.java
+++ hello/src/test/java/ClassloaderTest.java
@@ -1,11 +1,20 @@
+import net.sf.ehcache.config.CacheConfiguration;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 /**
  * Created by thomas on 26/09/14.
  */
 public class ClassloaderTest {
 
-    @ClassloaderTest
-    public void test() {
-
+    @Test
+    public void test() throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("net.sf.ehcache.config.CacheConfiguration");
+        Class<?> clazz2 = Class.forName("net.sf.ehcache.config.CacheConfiguration");
+        assertEquals(clazz, clazz2);
+        assertSame(clazz, clazz2);
     }
 
 }
