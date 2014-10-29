a9a4c94ee0e7b0d822ad197c7349f5a843cf5df3
Thomas Johansen <thxmasj@gmail.com>
2014-10-29 17:01:48 +0100

    STS now packaged in a single JAR. Keystore path is now specified through standard javax.net.ssl.keyStore sysprop Keystore password is now specified through standard javax.net.ssl.keyStorePassword sysprop, alternatively read from console.

--- sts/src/main/java/it/thomasjohansen/sts/launch/TomcatLauncher.java
+++ sts/src/main/java/it/thomasjohansen/sts/launch/TomcatLauncher.java
@@ -1,20 +1,19 @@
 package it.thomasjohansen.sts.launch;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.Wrapper;
+import org.apache.catalina.*;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.web.context.ContextLoaderListener;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletException;
 import java.io.File;
+import java.io.IOException;
+import java.security.UnrecoverableKeyException;
+import java.util.Scanner;
 
 public class TomcatLauncher {
 
-    public static void main(String[] args) throws LifecycleException, ServletException {
+    public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
         Context context = tomcat.addContext("/sts", "/tmp");
         context.addApplicationListener(ContextLoaderListener.class.getName());
@@ -29,8 +28,17 @@ public class TomcatLauncher {
         connector.setPort(8443);
         connector.setSecure(true);
         connector.setScheme("https");
-        connector.setAttribute("keystoreFile", "sts-ssl.jks");
-        connector.setAttribute("keystorePass", "changeit");
+        //connector.setAttribute("ciphers", "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH+aRSA+RC4:EECDH:EDH+aRSA:RC4:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS");
+        if (System.getProperty("javax.net.ssl.keyStore") == null)
+            throw new IllegalArgumentException("System property \"javax.net.ssl.keyStore\" must specify the path to a JKS keystore");
+        connector.setAttribute("keystoreFile", new File(System.getProperty("javax.net.ssl.keyStore")).getAbsolutePath());
+        String keystorePassword = System.getProperty("javax.net.ssl.keyStorePassword");
+        if (keystorePassword == null) {
+            if (System.console() == null)
+                throw new IllegalArgumentException("System property \"javax.net.ssl.keyStorePassword\" must specify a keystore password");
+            keystorePassword = String.valueOf(System.console().readPassword("Enter keystore password: ", null));
+        }
+        connector.setAttribute("keystorePass", keystorePassword);
         connector.setAttribute("clientAuth", false);
         connector.setAttribute("SSLEnabled", "true");
         connector.setAttribute("sslProtocol", "TLS");
