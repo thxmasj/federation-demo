32ed03769923f2a2039a124dd4ba2c1fab8cf8fa
Thomas Johansen <thxmasj@gmail.com>
2014-10-30 14:59:11 +0100

    Packaging with one-jar.  Specified ciphers on SSL connection

--- sts/src/main/java/it/thomasjohansen/sts/launch/TomcatLauncher.java
+++ sts/src/main/java/it/thomasjohansen/sts/launch/TomcatLauncher.java
@@ -1,15 +1,16 @@
 package it.thomasjohansen.sts.launch;
 
-import org.apache.catalina.*;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
 import org.apache.catalina.connector.Connector;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.web.context.ContextLoaderListener;
 
 import java.io.File;
-import java.io.IOException;
-import java.security.UnrecoverableKeyException;
-import java.util.Scanner;
+import java.nio.file.FileSystems;
 
 public class TomcatLauncher {
 
@@ -24,11 +25,12 @@ public class TomcatLauncher {
         wrapper.setLoadOnStartup(1);
         context.addChild(wrapper);
         context.addServletMapping("/*", "sts");
+        context.setLoader(new WebappLoader(Thread.currentThread().getContextClassLoader()));
         Connector connector = new Connector();
         connector.setPort(8443);
         connector.setSecure(true);
         connector.setScheme("https");
-        //connector.setAttribute("ciphers", "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH+aRSA+RC4:EECDH:EDH+aRSA:RC4:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS");
+        connector.setAttribute("ciphers", "ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM");
         if (System.getProperty("javax.net.ssl.keyStore") == null)
             throw new IllegalArgumentException("System property \"javax.net.ssl.keyStore\" must specify the path to a JKS keystore");
         connector.setAttribute("keystoreFile", new File(System.getProperty("javax.net.ssl.keyStore")).getAbsolutePath());
@@ -41,7 +43,8 @@ public class TomcatLauncher {
         connector.setAttribute("keystorePass", keystorePassword);
         connector.setAttribute("clientAuth", false);
         connector.setAttribute("SSLEnabled", "true");
-        connector.setAttribute("sslProtocol", "TLS");
+        connector.setAttribute("sslEnabledProtocols", "TLSv1.2");
+        connector.setAttribute("sslProtocol", "TLSv1.2");
         tomcat.getService().addConnector(connector);
         tomcat.setConnector(connector);
         tomcat.start();
